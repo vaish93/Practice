3b89d551922c65317a4fc1f463f0385ed8a845eb
vaish93 <60688893+vaish93@users.noreply.github.com>
2023-07-01 05:15:58 +0530

    Add files via upload

---
+++ alternate_word_reversal.py
@@ -0,0 +1,19 @@
+mystr = "Vaishali is a Automation tester"
+list1 = mystr.split(" ")
+len_mystr = len(list1)
+print(len_mystr)
+output = ""
+for i in range(len_mystr):
+    if i%2 == 1:
+        word = list1[i]
+        list2 = list(word)
+        rev_word=[]
+        for letter in list2:
+            rev_word.insert(0,letter)
+
+        rev_word_final = "".join(rev_word)
+    else:
+        rev_word_final =  list1[i]
+    output = output + " " + rev_word_final
+    #output = f'{output,rev_word_final}'
+print(output)
