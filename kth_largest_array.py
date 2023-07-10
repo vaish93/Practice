9d798954e38c2d73245b4bbedecc31143ffc8046
vaish93 <60688893+vaish93@users.noreply.github.com>
2023-07-10 18:26:03 +0530

    Add files via upload

---
+++ kth_largest_array.py
@@ -0,0 +1,10 @@
+arr = [3,1,6,109,23,45,11]
+k = 1
+
+#sort
+
+arr.sort()   # 1  3   6   11 23  45
+
+print(arr[len(arr)-k])
+
+
---
+++ kth_smallest_arraay.py
@@ -0,0 +1,10 @@
+arr = [4,1,7,2,100,23]
+k= 5
+
+#sort
+
+arr.sort()
+
+print(arr[k-1])
+
+
