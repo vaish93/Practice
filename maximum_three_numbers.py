02537fec9f2ce5f75334851a49e599ce428a68e7
vaish93 <60688893+vaish93@users.noreply.github.com>
2023-08-23 14:20:18 +0530

    Create maximum_three_numbers.py

---
+++ maximum_three_numbers.py
@@ -0,0 +1,12 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+a = 100 
+b = 23 
+c = 90
+
+if (a>b) and (a>c):
+    print("A is greater")
+elif (b>a) and (b>c):
+    print("B is greater")
+else:
+    print("C is greater")
