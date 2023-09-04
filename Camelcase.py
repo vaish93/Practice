20afd1d53df35e28d2b36c0d2e8cf69f90604852
vaish93 <60688893+vaish93@users.noreply.github.com>
2023-09-04 13:18:20 +0530

    Create Camelcase.py

---
+++ Camelcase.py
@@ -0,0 +1,18 @@
+myline = "my name is vaishali"
+
+mylist = myline.split(" ")
+
+mylist1 = []
+
+for i in range(len(mylist)):
+    letterlist = list(mylist[i])
+    letters = []
+    for i in range(len(letterlist)):
+        if i == 0:
+            d = letterlist[i].upper()  
+        else: 
+            d= letterlist[i]
+        letters.append(d)
+    final_word = "".join(letters)
+    mylist1.insert(0,final_word)
+print(" ".join(mylist1))
